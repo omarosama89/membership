a65e7e2db578f119b3846109bbc48af5854543e2
Omar <o.mohammed@morniksa.com>
2018-12-28 14:43:43 +0200

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+# Add admin user
 User.create(email: 'admin@honestbee.com', password: '12345678', password_confirmation: '12345678', role: 'admin')
+
+# Add 2 normal user
 User.create(email: 'normal@honestbee.com', password: '12345678', password_confirmation: '12345678', role: 'normal')
+user = User.create(email: 'normal2@honestbee.com', password: '12345678', password_confirmation: '12345678', role: 'normal')
+
+# Add premium membership for user `normal2@honestbee.com`
+PremiumMembership.create(user: user)
+
+# Add 6 products
+(0..11).to_a.each do |idx|
+  Product.create(name: "product-#{idx}", price: (rand() * 100).to_f.round(2), discount: (rand * 100).to_i, images: (0..9).map(&:to_s).map{|x| "p#{x}.jpg"}.map{|x| Rack::Test::UploadedFile.new(Rails.root.join("public/products_images/#{x}"), 'image/jpeg')}.sample([3,4,5,6,7].sample))
+end
